0adc5bea70bcafdfdb441a9d67f2c65853d08249
mxdillon <marowidi@gmail.com>
2020-01-06 18:01:03 +0000

    feat: added initial app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+# Step 1: Set up target metrics for evaluating training
+
+# Define a target loss metric to aim for
+target_loss = 0.26
+target_accuracy = 90
+
+# Step 2: Perform training for the model
+
